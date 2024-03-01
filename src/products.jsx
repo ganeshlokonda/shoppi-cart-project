158c3ffefc9a6bf023391c439f81a5fee38414fb
lokonda ganesh <lokondaganesh580@gmail.com>
2024-03-01 22:45:53 +0530

    first commit

---
+++ src/products.jsx
@@ -0,0 +1,80 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.jpg";
+import product3 from "./assets/products/3.jpg";
+import product4 from "./assets/products/4.jpg";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.jpg";
+import product7 from "./assets/products/7.jpg";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.jpg";
+import product10 from "./assets/products/10.jpg";
+import product11 from "./assets/products/11.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "oneplus",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "redmi",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "lenovo",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "kids dress",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "dress for womens",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "shirt",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "teddy",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "car",
+    price: 40.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "teddy frog",
+    price: 40.0,
+    productImage: product10,
+  },
+  {
+    id: 8,
+    productName: "teddy bear",
+    price: 40.0,
+    productImage: product11,
+  },
+];
